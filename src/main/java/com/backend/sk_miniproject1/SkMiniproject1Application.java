43023f87c242e57b1a8e6baa2611879294bfda5f
pyosang0919@gmail.com <hyuk5267!@#>
2025-01-06 15:09:28 +0900

    test

---
+++ src/main/java/com/backend/sk_miniproject1/SkMiniproject1Application.java
@@ -0,0 +1,13 @@
+package com.backend.sk_miniproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkMiniproject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkMiniproject1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/sk_miniproject1/SkMiniproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.sk_miniproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkMiniproject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

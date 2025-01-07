76e3e6fd58fc1da8a0e13c60c34a63af60cca22b
K-YG <kukn2002@gmail.com>
2025-01-07 13:15:25 +0900

    <pm.01:15 / UserRepository 오류 해결>

--- src/main/java/com/backend/repository/UserRepository.java
+++ src/main/java/com/backend/repository/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * User 엔티티에서 닉네임을 기준으로 사용자를 조회하는 메서드
  */
 
-public class UserRepository extends JpaRepository<User, Long> {
+// JpaRepository => 인터페이스라서 'UserRepository'를 class로 하면 오류 발생
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByNickname(String nickname);
 }
